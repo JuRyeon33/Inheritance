68eb67be9d7d283ee77d05334317a7af13b1eda7
JuRyeon33 <thwnfus0330@gmail.com>
2025-06-12 10:09:25 +0900

    Inheritance Animal-Dog

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,22 @@
 class Animal {
     String name;
+
+    public Animal(String name) {
+        this.name = name;
+    }
 }
 
 class Dog extends Animal {
     String bread;
+
+    public Dog(String name, String bread) {
+        super(name);
+        this.bread = bread;
+    }
 }
 
 public class Main {
     public static void main(String[] args) {
-        Animal dog = new Dog();
+        Dog dog = new Dog();
     }
 }
