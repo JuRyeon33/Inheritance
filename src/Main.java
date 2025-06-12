46da5cd7ba7ddfc8f06562e6260b3d49ea5476fd
JuRyeon33 <thwnfus0330@gmail.com>
2025-06-12 09:45:07 +0900

    Inheritance

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+class Animal {
+    String name;
+}
+
+class Dog extends Animal {
+    String bread;
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Animal dog = new Dog();
+    }
+}
